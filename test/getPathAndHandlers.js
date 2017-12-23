27086fb6fadd189a5be7192f8f3321be96c992a2
Ashok <coderatlabs@gmail.com>
2017-12-23 11:48:13 +0530

    rename vars

--- test/getPathAndHandlers.js
+++ test/getPathAndHandlers.js
@@ -1,14 +1,14 @@
 const { test } = require('tap') 
 const { join, resolve } = require('path')
-const getRouteAndHandlers = require('../lib/getRouteAndHandlers');
+const getPathAndHandlers = require('../lib/getPathAndHandlers');
 
 test('should return obj with route and handler property', t => {
   const dirpath = join(__dirname, '../fixtures')
   const filepath = join(__dirname, '../fixtures/testdir/foo.js')
 
-  const o = getRouteAndHandlers(dirpath, filepath)
+  const o = getPathAndHandlers(dirpath, filepath)
 
-  t.ok(o.route)
+  t.ok(o.path)
   t.ok(o.handler)
   t.end()
 
@@ -18,7 +18,7 @@ test('should return empty obj in handler when file is empty', t => {
   const dirpath = join(__dirname, '../fixtures')
   const filepath = join(__dirname, '../fixtures/testdir/empty.js')
 
-  const o = getRouteAndHandlers(dirpath, filepath)
+  const o = getPathAndHandlers(dirpath, filepath)
 
   t.same(o.handler, {})
   t.end()
@@ -29,9 +29,9 @@ test('route should start with slash and should not have .js extension', t => {
   const dirpath = join(__dirname, '../fixtures')
   const filepath = join(__dirname, '../fixtures/testdir/foo.js')
 
-  const o = getRouteAndHandlers(dirpath, filepath)
+  const o = getPathAndHandlers(dirpath, filepath)
 
-  t.same(o.route, '/fixtures/testdir/foo')
+  t.same(o.path, '/fixtures/testdir/foo')
   t.ok(o.handler)
   t.end()
 
